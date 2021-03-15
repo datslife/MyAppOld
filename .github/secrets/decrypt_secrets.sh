d70348d0183c18b14c685c6af3e6bd9db08d080e
datslife <datslife0601@gmail.com>
2021-03-15 22:52:05 +0700

    Update ci file

--- .github/secrets/decrypt_secrets.sh
+++ .github/secrets/decrypt_secrets.sh
@@ -7,7 +7,13 @@ mkdir -p ~/Library/MobileDevice/Provisioning\ Profiles
 
 cp ./.github/secrets/profile.mobileprovision ~/Library/MobileDevice/Provisioning\ Profiles/86ce4d81-fd7e-46b2-ae6a-3092b4af6cd7.mobileprovision
 
+KEYCHAIN_PATH=$RUNNER_TEMP/build
 
+security create-keychain -p ""> $KEYCHAIN_PATH
+security set-keychain-settings -lut 21600 $KEYCHAIN_PATH
+security unlock-keychain -p ""> $KEYCHAIN_PATH
+security import Certificates.p12 -P <p12 password> -A -t cert -f pkcs12 -k $KEYCHAIN_PATH
+security list-keychain -d user -s $KEYCHAIN_PATH
 security create-keychain -p "" build.keychain
 security import ./.github/secrets/Certificates.p12 -t agg -k ~/Library/Keychains/build.keychain -P "" -A
 
