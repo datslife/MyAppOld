99757705eb9ad1b5913d7dcea1630903e8278fe9
datslife <datslife0601@gmail.com>
2021-03-19 23:38:56 +0700

    Update ci file

--- .github/secrets/decrypt_secrets.sh
+++ .github/secrets/decrypt_secrets.sh
@@ -7,18 +7,18 @@ mkdir -p ~/Library/MobileDevice/Provisioning\ Profiles
 
 cp ./.github/secrets/profile.mobileprovision ~/Library/MobileDevice/Provisioning\ Profiles/86ce4d81-fd7e-46b2-ae6a-3092b4af6cd7.mobileprovision
 
-KEYCHAIN_PATH=$RUNNER_TEMP/build
+KEYCHAIN_PATH=$RUNNER_TEMP/build.keychain
 
 security create-keychain -p ""> $KEYCHAIN_PATH
-security set-keychain-settings -lut 21600 $KEYCHAIN_PATH
-security unlock-keychain -p ""> $KEYCHAIN_PATH
-security import Certificates.p12 -P <p12 password> -A -t cert -f pkcs12 -k $KEYCHAIN_PATH
-security list-keychain -d user -s $KEYCHAIN_PATH
-security create-keychain -p "" build.keychain
-security import ./.github/secrets/Certificates.p12 -t agg -k ~/Library/Keychains/build.keychain -P "" -A
+# security set-keychain-settings -lut 21600 $KEYCHAIN_PATH
+# security unlock-keychain -p ""> $KEYCHAIN_PATH
+# security import Certificates.p12 -P <p12 password> -A -t cert -f pkcs12 -k $KEYCHAIN_PATH
+# security list-keychain -d user -s $KEYCHAIN_PATH
+# security create-keychain -p "" build.keychain
+security import ./.github/secrets/Certificatesp12. -t agg -k $KEYCHAIN_PATH -P "" -A
 
-security list-keychains -s ~/Library/Keychains/build.keychain
-security default-keychain -s ~/Library/Keychains/build.keychain
-security unlock-keychain -p "" ~/Library/Keychains/build.keychain
+security list-keychains -s $KEYCHAIN_PATH
+security default-keychain -s $KEYCHAIN_PATH
+security unlock-keychain -p "" $KEYCHAIN_PATH
 
-security set-key-partition-list -S apple-tool:,apple: -s -k "" ~/Library/Keychains/build.keychain
+security set-key-partition-list -S apple-tool:,apple: -s -k "" $KEYCHAIN_PATH
