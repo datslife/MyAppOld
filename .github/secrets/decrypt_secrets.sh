87d09936b303a5c75faa98dcfab9df779d2f2d93
datslife <datslife0601@gmail.com>
2021-03-20 23:39:07 +0700

    Update ci file

--- .github/secrets/decrypt_secrets.sh
+++ .github/secrets/decrypt_secrets.sh
@@ -1,11 +1,11 @@
 # #!/bin/sh
 
-# gpg --quiet --batch --yes --decrypt --passphrase="$IOS_PROFILE_KEY" --output ./.github/secrets/profile.mobileprovision ./.github/secrets/profile.mobileprovision.gpg
-# gpg --quiet --batch --yes --decrypt --passphrase="$IOS_PROFILE_KEY" --output ./.github/secrets/Certificates.p12 ./.github/secrets/Certificates.p12.gpg
+gpg --quiet --batch --yes --decrypt --passphrase="$IOS_PROFILE_KEY" --output ./.github/secrets/profile.mobileprovision ./.github/secrets/profile.mobileprovision.gpg
+gpg --quiet --batch --yes --decrypt --passphrase="$IOS_PROFILE_KEY" --output ./.github/secrets/Certificates.p12 ./.github/secrets/Certificates.p12.gpg
 
-# mkdir -p ~/Library/MobileDevice/Provisioning\ Profiles
+mkdir -p ~/Library/MobileDevice/Provisioning\ Profiles
 
-# cp ./.github/secrets/profile.mobileprovision ~/Library/MobileDevice/Provisioning\ Profiles/86ce4d81-fd7e-46b2-ae6a-3092b4af6cd7.mobileprovision
+cp ./.github/secrets/profile.mobileprovision ~/Library/MobileDevice/Provisioning\ Profiles/86ce4d81-fd7e-46b2-ae6a-3092b4af6cd7.mobileprovision
 
 # KEYCHAIN_PATH=$RUNNER_TEMP/build.keychain
 
@@ -23,24 +23,44 @@
 
 # security set-key-partition-list -S apple-tool:,apple: -s -k "" $KEYCHAIN_PATH
 
-KEYCHAIN_PATH=$RUNNER_TEMP/build.keychain
+# KEYCHAIN_PATH=$RUNNER_TEMP/build.keychain
 
-set -eo pipefail
+# set -eo pipefail
 
-gpg --quiet --batch --yes --decrypt --passphrase="$IOS_PROFILE_KEY" --output ./.github/secrets/profile.mobileprovision ./.github/secrets/profile.mobileprovision.gpg
-gpg --quiet --batch --yes --decrypt --passphrase="$IOS_PROFILE_KEY" --output ./.github/secrets/Certificates.p12 ./.github/secrets/Certificates.p12.gpg
+# gpg --quiet --batch --yes --decrypt --passphrase="$IOS_PROFILE_KEY" --output ./.github/secrets/profile.mobileprovision ./.github/secrets/profile.mobileprovision.gpg
+# gpg --quiet --batch --yes --decrypt --passphrase="$IOS_PROFILE_KEY" --output ./.github/secrets/Certificates.p12 ./.github/secrets/Certificates.p12.gpg
 
-mkdir -p ~/Library/MobileDevice/Provisioning\ Profiles
+# mkdir -p ~/Library/MobileDevice/Provisioning\ Profiles
 
-cp ./.github/secrets/profile.mobileprovision ~/Library/MobileDevice/Provisioning\ Profiles/86ce4d81-fd7e-46b2-ae6a-3092b4af6cd7.mobileprovision
+# cp ./.github/secrets/profile.mobileprovision ~/Library/MobileDevice/Provisioning\ Profiles/86ce4d81-fd7e-46b2-ae6a-3092b4af6cd7.mobileprovision
 
-security create-keychain -p "" $KEYCHAIN_PATH
-security import ./.github/secrets/Certificates.p12 -t agg -k $KEYCHAIN_PATH -P "" -A
+# security create-keychain -p "" $KEYCHAIN_PATH
+# security import ./.github/secrets/Certificates.p12 -t agg -k $KEYCHAIN_PATH -P "" -A
 
-security list-keychains -s $KEYCHAIN_PATH
-security default-keychain -s $KEYCHAIN_PATH
-security unlock-keychain -p "" $KEYCHAIN_PATH
+# security list-keychains -s $KEYCHAIN_PATH
+# security default-keychain -s $KEYCHAIN_PATH
+# security unlock-keychain -p "" $KEYCHAIN_PATH
 
-security set-key-partition-list -S apple-tool:,apple: -s -k "" $KEYCHAIN_PATH
+# security set-key-partition-list -S apple-tool:,apple: -s -k "" $KEYCHAIN_PATH
 
-security find-identity -v -p codesigning
+# security find-identity -v -p codesigning
+
+# ====
+# security create-keychain -p "" build.keychain
+# security import ./.github/secrets/Certificates.p12 -t agg -k ~/Library/Keychains/build.keychain -P "" -A
+
+# security list-keychains -s ~/Library/Keychains/build.keychain
+# security default-keychain -s ~/Library/Keychains/build.keychain
+# security unlock-keychain -p "" ~/Library/Keychains/build.keychain
+
+# security set-key-partition-list -S apple-tool:,apple: -s -k "" ~/Library/Keychains/build.keychain
+
+
+security create-keychain -p "" build.keychain
+security import ./.github/secrets/Certificates.p12 -t agg -k ~/Library/Keychains/build.keychain -P "" -A
+
+security list-keychains -s ~/Library/Keychains/build.keychain
+security default-keychain -s ~/Library/Keychains/build.keychain
+security unlock-keychain -p "" ~/Library/Keychains/build.keychain
+
+security set-key-partition-list -S apple-tool:,apple: -s -k "" ~/Library/Keychains/build.keychain
