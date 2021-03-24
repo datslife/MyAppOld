d54d35a601954b77bb0dace41ddb6ac96e370a95
datslife <datslife0601@gmail.com>
2021-03-24 13:34:04 +0700

    Update ci file

--- .github/secrets/decrypt_secrets.sh
+++ .github/secrets/decrypt_secrets.sh
@@ -8,6 +8,8 @@ mkdir -p ~/Library/MobileDevice/Provisioning\ Profiles
 cp ./.github/secrets/profile.mobileprovision ~/Library/MobileDevice/Provisioning\ Profiles/86ce4d81-fd7e-46b2-ae6a-3092b4af6cd7.mobileprovision
 
 # Create temporary keychain
+# I added the $$ which is the PID.
+# This means we allow can parallelize the script by allowing multiple temporary keychains to be created concurrently
 KEYCHAIN="MyApp$$.keychain"
 KEYCHAIN_PASSWORD="MyApp"
 
@@ -23,7 +25,12 @@ security list-keychains
 security set-keychain-settings "$KEYCHAIN"
 security unlock-keychain -p "$KEYCHAIN_PASSWORD" "$KEYCHAIN"
 
-# Import certificate
+# Import the private key/certificate (*.p12) for your CodeSign identity
+# The key here is the "-A" flag.
+# This will allow access to the keychain without warning.
+# This is why you're seeing the "User interaction is not allowed" error.
+# If you were attempting this build via the Xcode UI,
+# this is the point where it would prompt you to "Allow access" to your keychain.
 # security import ./.github/secrets/Certificates.p12 -k "$KEYCHAIN" -P "" -T "/usr/bin/codesign"
 security import ./.github/secrets/Certificates.p12 -t agg -k $KEYCHAIN -P "" -A
 
